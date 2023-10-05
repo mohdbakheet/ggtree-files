4fd9de8920191b76dcbb42ec714e6e9a99defa3e
mohdbakheet <35845601+mohdbakheet@users.noreply.github.com>
2023-10-05 19:36:04 -0400

    Update ggtree-code.R

--- ggtree-code.R
+++ ggtree-code.R
@@ -1,13 +1,8 @@
 library(ggplot2)
 library(ggtree)
 library(treeio)
-# Lambodhar Damodaran 
-# Visualize multiple MCC trees together
 
 beast_tree <- read.beast("final-tree.tre")
-
-
-##################################################
 p1 <-  ggtree(beast_tree, mrsd='1990-01-01') +
   theme_tree2(panel.grid.major.x=element_line()) + 
   ggtitle("    HA") +
@@ -15,13 +10,5 @@ p1 <-  ggtree(beast_tree, mrsd='1990-01-01') +
   geom_tippoint(aes(subset = (node == 303)), , size=3, shape=23, fill="steelblue") +
   geom_highlight(node = 311 , fill = "red")  +
   scale_x_ggtree(breaks = c(1973.5,1979,1984.5,1990))
-
 p1
-
-###################################################
-
-
-####################################################
-
-
 cowplot::plot_grid(p1, nrow=1, labels = LETTERS[1:2])
